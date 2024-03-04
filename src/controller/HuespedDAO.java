7c59e30ddbc5530cfefc8fadc152f79b2e70fb93
S_Uboldi <solmaruboldi@gmail.com>
2024-03-04 12:32:41 -0300

    Corregí errores en controller

--- src/controller/HuespedDAO.java
+++ src/controller/HuespedDAO.java
@@ -1,11 +1,13 @@
-package model;
+package controller;
 
 import java.sql.Connection;
+
+import model.Huesped;
 
 public class HuespedDAO {
 	
 	public static void crearHuespedDB(Huesped huesped) {
-		//Connexion db_connexion = new Connexion();
+		Connexion db_connexion = new Connexion();
 	}
 
 }
