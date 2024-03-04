6297acf4a3313788d03b0f6367672456cbd6302e
S_Uboldi <solmaruboldi@gmail.com>
2024-03-04 11:12:34 -0300

    Agregue las clases Huespedes y Reservas al modelo

---
+++ src/model/Huespedes.java
@@ -0,0 +1,87 @@
+package model;
+
+public class Huespedes {
+	
+	int id;
+	String nombre;
+	String apellido;
+	String fecha_de_nacimiento;
+	String nacionalidad;
+	int telefono;
+	int id_reserva;
+	
+	public Huespedes() {
+	}
+
+	public Huespedes(int id, String nombre, String apellido, String fecha_de_nacimiento, String nacionalidad,
+			int telefono, int id_reserva) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fecha_de_nacimiento = fecha_de_nacimiento;
+		this.nacionalidad = nacionalidad;
+		this.telefono = telefono;
+		this.id_reserva = id_reserva;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getFecha_de_nacimiento() {
+		return fecha_de_nacimiento;
+	}
+
+	public void setFecha_de_nacimiento(String fecha_de_nacimiento) {
+		this.fecha_de_nacimiento = fecha_de_nacimiento;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public int getId_reserva() {
+		return id_reserva;
+	}
+
+	public void setId_reserva(int id_reserva) {
+		this.id_reserva = id_reserva;
+	}
+	
+	
+	
+	
+
+}
---
+++ src/model/Reservas.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Reservas {
+	
+	int id;
+	String fecha_entrada;
+	String fecha_salida;
+	double valor;
+	String forma_pago;
+	
+	public Reservas() {
+	}
+	
+	public Reservas(int id, String fecha_entrada, String fecha_salida, double valor, String forma_pago) {
+		this.id = id;
+		this.fecha_entrada = fecha_entrada;
+		this.fecha_salida = fecha_salida;
+		this.valor = valor;
+		this.forma_pago = forma_pago;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFecha_entrada() {
+		return fecha_entrada;
+	}
+	public void setFecha_entrada(String fecha_entrada) {
+		this.fecha_entrada = fecha_entrada;
+	}
+	public String getFecha_salida() {
+		return fecha_salida;
+	}
+	public void setFecha_salida(String fecha_salida) {
+		this.fecha_salida = fecha_salida;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public String getForma_pago() {
+		return forma_pago;
+	}
+	public void setForma_pago(String forma_pago) {
+		this.forma_pago = forma_pago;
+	}
+	
+}
